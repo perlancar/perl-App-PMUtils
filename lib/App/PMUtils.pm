6c6360fcd359fd745c75b189b1cdeaa4e06349a0
Steven Haryanto (on PC, Bandung) <stevenharyanto@gmail.com>
2014-06-24 01:29:58 +0700

    Initial commit (unfinished)

---
+++ lib/App/PMUtils.pm
@@ -0,0 +1,21 @@
+package App::PMUtils;
+
+use 5.010001;
+use strict;
+use warnings;
+
+# VERSION
+# DATE
+
+1;
+# ABSTRACT: Command line to manipulate Perl module files
+
+=head1 SYNOPSIS
+
+This distribution provides the following command-line utilities:
+
+ pmpath
+ pmless
+ pmedit
+
+=cut
