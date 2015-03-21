0ca3358866a7b0a1111bcb8db58018da807228ab
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2015-03-21 12:25:14 +0700

    [doc] Make a complete & reusable list of similar distributions and faq for other dists to use

--- lib/App/PMUtils.pm
+++ lib/App/PMUtils.pm
@@ -33,49 +33,57 @@ our $arg_module_single = {
 
 =head1 SYNOPSIS
 
-This distribution provides several command-line utilities related to Perl
-modules. The main purpose of these utilities is tab completion.
+This distribution provides the following command-line utilities related to Perl
+modules:
+
+The main purpose of these utilities is tab completion.
 
 
 =head1 FAQ
 
-=head2 What is the purpose of this distribution? Haven't other utilities existed?
+=for BEGIN_BLOCK: faq
 
-For example, L<mpath> in L<Module::Path> distribution, L<mversion> in
-L<Module::Version> distribution, etc.
+=head2 What is the purpose of this distribution? Haven't other similar utilities existed?
+
+For example, L<mpath> from L<Module::Path> distribution is similar to L<pmpath>
+in L<App::PMUtils>, and L<mversion> from L<Module::Version> distribution is
+similar to L<pmversion> from L<App::PMUtils> distribution, and so on.
 
 True. The main point of these utilities is shell tab completion, to save
 typing.
 
-=head2 For shell completion, why do you recommend the use of / (slash) instead of :: (double colon) as it should be?
-
-Colon is problematic because by default it is a word breaking character in bash.
-This means, in this command:
-
- % pmpath Text:<tab>
-
-bash is completing a new word (empty string), and in this:
-
- % pmpath Text::ANSITabl<tab>
-
-bash is completing C<ANSITabl> instead of what we want C<Text::ANSITabl>.
-
-The solution is to use quotes, e.g.
-
- % pmpath "Text:<tab>
- % pmpath 'Text:<tab>
-
-or, use /, which is by default not a word-breaking character in bash.
-
-The utilities are a bit smart: they accept both kinds of separator. If you type
-module name which doesn't contain any ::, / will be used as namespace separator;
-otherwise if you already type ::, it will use :: as the separator.
+=for END_BLOCK: faq
 
 
 =head1 SEE ALSO
 
-L<App::PlUtils>, distribution that provides utilities related to Perl scripts.
+=for BEGIN_BLOCK: see_also
 
-L<App::ProgUtils>, a similarly spirited distribution.
+Below is the list of distributions that provide CLI utilities for various
+purposes, with the focus on providing shell tab completion feature.
+
+L<App::DistUtils>, utilities related to Perl distributions.
+
+L<App::DzilUtils>, utilities related to L<Dist::Zilla>.
+
+L<App::GitUtils>, utilities related to git.
+
+L<App::IODUtils>, utilities related to L<IOD> configuration files.
+
+L<App::LedgerUtils>, utilities related to Ledger CLI files.
+
+L<App::OrgUtils>, utilities related to Org files.
+
+L<App::PlUtils>, utilities related to Perl scripts.
+
+L<App::PMUtils>, utilities related to Perl modules.
+
+L<App::ProgUtils>, utilities related to programs.
+
+L<App::UpfUtils>, utilities related to Unix password/shadow files.
+
+L<App::WeaverUtils>, utilities related to L<Pod::Weaver>.
+
+=for END_BLOCK: see_also
 
 =cut
