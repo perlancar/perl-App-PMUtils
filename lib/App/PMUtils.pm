d93ce9d582c9d584e3b8a3533129936db302d036
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2016-01-09 09:24:31 +0700

    Add script: pmdir, add pro-tip about cdpm shell function

--- lib/App/PMUtils.pm
+++ lib/App/PMUtils.pm
@@ -5,6 +5,8 @@ package App::PMUtils;
 
 use 5.010001;
 
+our %SPEC;
+
 our $arg_module_multiple = {
     schema => ['array*' => of=>'str*', min_len=>1],
     req    => 1,
@@ -27,6 +29,117 @@ our $arg_module_single = {
         Complete::Module::complete_module(word=>$args{word});
     },
 };
+
+$SPEC{pmpath} = {
+    v => 1.1,
+    summary => 'Get path to locally installed Perl module',
+    args => {
+        module => $App::PMUtils::arg_module_multiple,
+        all => {
+            summary => 'Return all found files for each module instead of the first one',
+            schema => 'bool',
+            cmdline_aliases => {a=>{}},
+        },
+        abs => {
+            summary => 'Absolutify each path',
+            schema => 'bool',
+            cmdline_aliases => {P=>{}},
+        },
+        pm => {
+            schema => 'bool',
+            default => 1,
+        },
+        pmc => {
+            schema => 'bool',
+            default => 0,
+        },
+        pod => {
+            schema => 'bool',
+            default => 0,
+        },
+        prefix => {
+            schema => 'bool',
+            default => 0,
+        },
+        dir => {
+            summary => 'Show directory instead of path',
+            description => <<'_',
+
+Also, will return `.` if not found, so you can conveniently do this on a Unix
+shell:
+
+    % cd `pmpath -Pd Moose`
+
+and it won't change directory if the module doesn't exist.
+
+_
+            schema  => ['bool', is=>1],
+            cmdline_aliases => {d=>{}},
+        },
+    },
+};
+sub pmpath {
+    require Module::Path::More;
+    my %args = @_;
+
+    my $mods = $args{module};
+    my $res = [];
+    my $found;
+
+    for my $mod (@{$mods}) {
+        my $mpath = Module::Path::More::module_path(
+            module      => $mod,
+            find_pm     => $args{pm},
+            find_pmc    => $args{pmc},
+            find_pod    => $args{pod},
+            find_prefix => $args{prefix},
+            abs         => $args{abs},
+            all         => $args{all},
+        );
+        $found++ if $mpath;
+        for (ref($mpath) eq 'ARRAY' ? @$mpath : ($mpath)) {
+            if ($args{dir}) {
+                require File::Spec;
+                my ($vol, $dir, $file) = File::Spec->splitpath($_);
+                $_ = $dir;
+            }
+            push @$res, @$mods > 1 ? {module=>$mod, path=>$_} : $_;
+        }
+    }
+
+    if ($found) {
+        [200, "OK", $res];
+    } else {
+        if ($args{dir}) {
+            [200, "OK (not found)", "."];
+        } else {
+            [404, "No such module"];
+        }
+    }
+}
+
+$SPEC{pmdir} = do {
+    my $meta = { %{ $SPEC{pmpath} } }; # shallow copy
+    $meta->{summary} = "Get directory of locally installed Perl module/prefix";
+    $meta->{description} = <<'_';
+
+This is basically a shortcut for:
+
+    % pmpath -Pd MODULE_OR_PREFIX_NAME
+
+Sometimes I forgot that `pmpath` has a `-d` option, and often intuitively look
+for a `pmdir` command.
+
+_
+    $meta->{args} = { %{ $SPEC{pmpath}{args} } }; # shalow copy
+    delete $meta->{args}{all};
+    delete $meta->{args}{dir};
+    delete $meta->{args}{prefix};
+    $meta;
+};
+sub pmdir {
+    pmpath(@_, prefix=>1, dir=>1);
+}
 
 1;
 # ABSTRACT: Command-line utilities related to Perl modules
